d9c548e2f74e7c65c33a22be200d68098562015f
Gerson-Hernandez <gersonandree@hotmail.com>
2021-01-09 20:52:27 -0600

    FileManager

---
+++ src/FileManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gerso on 9/1/2021.
+//
+
+#include "FileManager.h"
---
+++ src/FileManager.h
@@ -0,0 +1,14 @@
+//
+// Created by gerso on 9/1/2021.
+//
+
+#ifndef PROYECTO_1_DATOS_FILEMANAGER_H
+#define PROYECTO_1_DATOS_FILEMANAGER_H
+
+
+class FileManager {
+
+};
+
+
+#endif //PROYECTO_1_DATOS_FILEMANAGER_H
