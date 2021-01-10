6bb4735d24978eafc885add10346893dd548ad06
LeoCh98 <leochaveslp@hotmail.com>
2021-01-09 20:21:54 -0600

    ...

---
+++ BinaryTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leoch on 09/01/2021.
+//
+
+#include "BinaryTree.h"
---
+++ BinaryTree.h
@@ -0,0 +1,14 @@
+//
+// Created by leoch on 09/01/2021.
+//
+
+#ifndef PROYECTO_1_DATOS_BINARYTREE_H
+#define PROYECTO_1_DATOS_BINARYTREE_H
+
+
+class BinaryTree {
+
+};
+
+
+#endif //PROYECTO_1_DATOS_BINARYTREE_H
