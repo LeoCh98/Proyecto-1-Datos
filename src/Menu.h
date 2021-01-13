da7c48668648bae4a0290609b23eedb1da8d2d6e
Gerson-Hernandez <gersonandree@hotmail.com>
2021-01-12 21:44:37 -0600

    Menuu

---
+++ src/Menu.h
@@ -0,0 +1,22 @@
+//
+// Created by gerso on 12/1/2021.
+//
+
+#ifndef PROYECTO_1_DATOS_MENU_H
+#define PROYECTO_1_DATOS_MENU_H
+
+#include <iostream>
+
+using namespace std;
+
+class Menu {
+public:
+    Menu();
+    ~Menu();
+
+    void DataMenu();
+
+};
+
+
+#endif //PROYECTO_1_DATOS_MENU_H
